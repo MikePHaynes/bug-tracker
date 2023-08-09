0e96413605f054231f19b0bdf10e4b8139c088e0
MikePHaynes <86630042+MikePHaynes@users.noreply.github.com>
2023-08-08 17:17:51 -0700

    Updated ticket controller and service.

--- src/main/java/com/mph/bugtracker/ticket/TicketController.java
+++ src/main/java/com/mph/bugtracker/ticket/TicketController.java
@@ -1,7 +1,9 @@
 package com.mph.bugtracker.ticket;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects/{projectId}/tickets")
@@ -11,5 +13,35 @@ public class TicketController {
 
     public TicketController(TicketService ticketService) {
         this.ticketService = ticketService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket) {
+        Ticket createdTicket = ticketService.createTicket(ticket);
+        return ResponseEntity.ok(createdTicket);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ticket>> getAllTickets() {
+        List<Ticket> tickets = ticketService.getAllTickets();
+        return ResponseEntity.ok(tickets);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ticket> getTicketById(@PathVariable Long id) {
+        Ticket ticket = ticketService.getTicketById(id);
+        return ResponseEntity.ok(ticket);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Ticket> updateTicketById(@PathVariable Long id,
+                                                   @RequestBody Ticket ticket) {
+        Ticket updatedTicket = ticketService.updateTicketById(id, ticket);
+        return ResponseEntity.ok(updatedTicket);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTicketById(@PathVariable Long id) {
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/mph/bugtracker/ticket/TicketService.java
+++ src/main/java/com/mph/bugtracker/ticket/TicketService.java
@@ -3,6 +3,8 @@ package com.mph.bugtracker.ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class TicketService {
 
@@ -11,5 +13,30 @@ public class TicketService {
     @Autowired
     public TicketService(TicketRepository ticketRepository) {
         this.ticketRepository = ticketRepository;
+    }
+
+    public Ticket createTicket(Ticket ticket) {
+        return ticketRepository.save(ticket);
+    }
+
+    public List<Ticket> getAllTickets() {
+        return ticketRepository.findAll();
+    }
+
+    public Ticket getTicketById(Long id) {
+        return ticketRepository.findById(id)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with ID: " + id));
+    }
+
+    public Ticket updateTicketById(Long id, Ticket updatedTicket) {
+        Ticket existingTicket = ticketRepository.findById(id)
+                .orElseThrow(() -> new TicketNotFoundException("Ticket not found with ID: " + id));
+
+        existingTicket.setTicketName(updatedTicket.getTicketName());
+        existingTicket.setTicketDescription(updatedTicket.getTicketDescription());
+        existingTicket.setSeverityLevel(updatedTicket.getSeverityLevel());
+
+        ticketRepository.save(existingTicket);
+        return existingTicket;
     }
 }
